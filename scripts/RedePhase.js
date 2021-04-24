c99fd9a9d9510faac9ac2512c0c68b9f392a3af5
Enoughdv <67561225+Enoughsdv@users.noreply.github.com>
2021-04-24 14:05:26 -0500

    Update RedePhase.js

--- scripts/RedePhase.js
+++ scripts/RedePhase.js
@@ -16,7 +16,7 @@ module = {
       if(mc.thePlayer.ticksExisted < 200 && mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY-2, mc.thePlayer.posZ)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY-1, mc.thePlayer.posZ)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX+1, mc.thePlayer.posY, mc.thePlayer.posZ)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX-1, mc.thePlayer.posY, mc.thePlayer.posZ)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ+1)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ-1)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY+1, mc.thePlayer.posZ+1)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY+1, mc.thePlayer.posZ-1)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX+1, mc.thePlayer.posY+1, mc.thePlayer.posZ)) && !mc.theWorld.isAirBlock(new BlockPos(mc.thePlayer.posX-1, mc.thePlayer.posY+1, mc.thePlayer.posZ))) {
         mc.gameSettings.keyBindForward.pressed = true;
         if(mc.thePlayer.isCollidedHorizontally) {
-        mc.thePlayer.motionY = -1;
+        mc.thePlayer.motionY = -0.4893472;
         mc.thePlayer.sendQueue.addToSendQueue(new C06PacketPlayerPosLook(mc.thePlayer.posX, mc.thePlayer.posY - 0.00000001, mc.thePlayer.posZ, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, false));
         mc.thePlayer.sendQueue.addToSendQueue(new C06PacketPlayerPosLook(mc.thePlayer.posX, mc.thePlayer.posY - 0.000001, mc.thePlayer.posZ, mc.thePlayer.rotationYaw, mc.thePlayer.rotationPitch, false));
         f = 1;
@@ -26,8 +26,4 @@ module = {
         else f--;
         if(f == 0) mc.gameSettings.keyBindForward.pressed = false;
     }
-}
-
-function onDisable() {
-	moduleManager.unregisterModule(client)
 }
